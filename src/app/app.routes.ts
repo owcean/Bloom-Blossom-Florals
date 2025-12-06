f7a2b00bfbbe6bdb8fe9a25e7bdbe9e182fc9063
owcean <tienzokrisean@gmail.com>
2025-12-07 01:48:18 +0800

    components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { EmployeeDirectory } from './employee-directory/employee-directory';
+import { About } from './about/about';
+import { Products } from './products/products';
+import { Pagenotfound } from './pagenotfound/pagenotfound';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'employee-directory', component: EmployeeDirectory},
+    {path: 'about', component: About},
+    {path: 'products', component: Products},
+    {path: '**', component: Pagenotfound}
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+<nav>
+  <a routerLink = "/">Home</a> |
+  <a routerLink = "/employee-directory">Employee</a> |
+  <a routerLink = "/about">About</a> |
+  <a routerLink = "/products">Products</a>
+</nav>
+<router-outlet />
